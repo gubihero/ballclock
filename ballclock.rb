157e6b5051461e8bcf45e1f6ee5010f4c670a9f9
James Gubler <gubimail@gmail.com>
2015-08-04 11:27:03 -0600

    made tray class to simplify cycle loop

--- ballclock.rb
+++ ballclock.rb
@@ -1,24 +1,35 @@
 #!/usr/bin/ruby
 
+class Tray
+  def initialize(size)
+    @size = size
+    @index = 0
+    @storage = Array.new(size,0)
+  end
 
-def tilt(array, ballqueue)
-  for i in (array.length-1).downto(0)
-    ballqueue.push(array[i])
-    array[i] = 0
+  def add_ball(ball)
+    return false if @index == @size
+    @storage[@index] = ball
+    @index += 1
+  end
+
+  def tilt(ballqueue)
+    for i in (@size-1).downto(0)
+      ballqueue.push(@storage[i])
+      @storage[i] = 0
+    end
+    @index = 0
   end
 end
 
 def cycleballs(balls)
-  mins = 0
-  fivemins = 0
-  hours = 0
   days = 0
 
   ballqueue = Queue.new
   (1..balls).each {|ball| ballqueue.push(ball)}
-  minarray = Array.new(4,0)
-  fiveminarray = Array.new(11,0)
-  hourarray = Array.new(11,0)
+  mintray = Tray.new(4)
+  fivemintray = Tray.new(11)
+  hourtray = Tray.new(11)
 
   dayflag = false
   
@@ -26,17 +37,14 @@ def cycleballs(balls)
     # pull ball up to min 
     ball = ballqueue.pop
     # see if tilts min
-    if mins == 4
-      mins = 0
-      tilt(minarray,ballqueue)
+    if !mintray.add_ball(ball)
+      mintray.tilt(ballqueue)
       # see if tilts 5 min
-      if fivemins == 11
-        fivemins = 0
-        tilt(fiveminarray,ballqueue)
+      if !fivemintray.add_ball(ball)
+        fivemintray.tilt(ballqueue)
         # see if tilts hours 
-        if hours == 11
-          hours = 0
-          tilt(hourarray,ballqueue)
+        if !hourtray.add_ball(ball)
+          hourtray.tilt(ballqueue)
           ballqueue.push(ball)
           # is full day??
           if dayflag
@@ -46,7 +54,6 @@ def cycleballs(balls)
             dayflag = true
           end
           # check if balls are in original order
-
           ballarray = Array.new(balls,0)
           queuelen = ballqueue.length 
           for i in 0..queuelen-1
@@ -57,20 +64,10 @@ def cycleballs(balls)
           if ballarray == ballarray.sort
             break
           end
-        else
-          hourarray[hours] = ball
-          hours += 1
         end
-      else
-        fiveminarray[fivemins] = ball
-        fivemins += 1
       end
-    else
-      minarray[mins] = ball
-      mins += 1
     end
   end
-
   return days
 end
 
@@ -84,6 +81,5 @@ while true do
   else
     puts "Invalid number of balls, please select a number between 27 and 127 inclusive"
   end
-  
 end
 
